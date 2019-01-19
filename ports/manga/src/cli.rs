c433f4573de352f8ed11446c31c264044e7ab359
Hentioe_Cl <me@bluerain.io>
2019-01-20 01:39:16 +0800

    The Author field of the CLI uses constants

--- ports/manga/src/cli.rs
+++ ports/manga/src/cli.rs
@@ -1,11 +1,13 @@
 use super::VERSION;
 use clap::{App, Arg};
 
+const AUTHOR: &'static str = "Hentioe Cl (绅士喵)";
+
 pub fn build_cli() -> App<'static, 'static> {
     App::new("manga")
         .version(VERSION)
         .about("An online manga(comic/album) export tool")
-        .author("Hentioe Cl (绅士喵)")
+        .author(AUTHOR)
         .arg(
             Arg::with_name("url")
                 .help("Online manga address")
@@ -24,6 +26,6 @@ pub fn build_cli() -> App<'static, 'static> {
             App::new("clean")
                 .version(VERSION)
                 .about("Empty the cache (the structure directory of epub)")
-                .author("Hentioe Cl (绅士喵)"),
+                .author(AUTHOR),
         )
 }
