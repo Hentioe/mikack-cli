c991461cb74d636769ab7279d47a5ec3d587baea
Hentioe_Cl <me@bluerain.io>
2019-01-28 10:58:41 +0800

    [Completed] use macros to simplify the addition of formats

--- ports/manga/src/formats.rs
+++ ports/manga/src/formats.rs
@@ -1,6 +1,7 @@
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 
+#[derive(Eq, PartialEq, Hash)]
 pub enum Format {
     Epub,
     Mobi,
@@ -14,48 +15,37 @@ use self::Format::*;
 macro_rules! append_formats {
     ( $( $format:expr ), * ) => {
         {
-            let mut formats: HashMap<&'static str, Format> = HashMap::new();
+            let mut formats_by_s: HashMap<&'static str, Format> = HashMap::new();
+            let mut formats_by_f: HashMap<Format, &'static str> = HashMap::new();
             $(
-                formats.insert($format.0, $format.1);
+                formats_by_s.insert($format.0, $format.1);
+                formats_by_f.insert($format.1, $format.0);
             )*
-            formats
+            (formats_by_s, formats_by_f)
         }
     }
 }
 
-lazy_static! {
-    static ref EPUB: &'static str = "epub";
-    static ref MOBI: &'static str = "mobi";
-    static ref AZW3: &'static str = "azw3";
-    static ref PDF: &'static str = "pdf";
-    static ref ZIP: &'static str = "zip";
-}
+type FormatBy = (HashMap<&'static str, Format>, HashMap<Format, &'static str>);
 
 lazy_static! {
-    static ref FORMATS: HashMap<&'static str, Format> = append_formats![
-        (*EPUB, Epub),
-        (*MOBI, Mobi),
-        (*AZW3, Azw3),
-        (*PDF, Pdf),
-        (*ZIP, Zip)
+    static ref FORMATS_BY: FormatBy = append_formats![
+        ("epub", Epub),
+        ("mobi", Mobi),
+        ("azw3", Azw3),
+        ("pdf", Pdf),
+        ("zip", Zip)
     ];
 }
 
 impl ToString for Format {
     fn to_string(&self) -> String {
-        match self {
-            Epub => *EPUB,
-            Mobi => *MOBI,
-            Azw3 => *AZW3,
-            Pdf => *PDF,
-            Zip => *ZIP,
-        }
-        .to_owned()
+        FORMATS_BY.1.get(self).unwrap().to_string()
     }
 }
 
 impl Format {
     pub fn find(format_s: &str) -> Option<&Format> {
-        FORMATS.get(format_s)
+        FORMATS_BY.0.get(format_s.to_lowercase().as_str())
     }
 }
